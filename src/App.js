cb934768983b44e5973104a5d7d9add1cccae446
Daniel Bauman <danielbauman1010@gmail.com>
2020-09-13 20:05:44 -0700

    added bootstrap and setup basic profile grid

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <h1>studygroup</h1>
+        <div className="navbar navbar-dark bg-primary">
+          <a href="/" className="navbar-brand">studygroup</a>
+        </div>
+        <div className="container-fluid">
+          <div className="profile">
+            <div className="row profile-row">
+              <div className="col-6">word</div>
+              <div className="col-6">word</div>
+            </div>
+
+            <div className="row profile-row">
+              <div className="col-6">word</div>
+              <div className="col-6">word</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
